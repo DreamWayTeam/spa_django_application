86e099746eca331843c99e33efa4fbf80f32e8d6
Igor Matchenko <igor@matchenko.com>
2020-04-13 18:45:17 +0300

    update about page

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path(
-        'about/', TemplateView.as_view(template_name='pages/about.html'), name='about'
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
--- spa/views.py
+++ spa/views.py
@@ -1,38 +1,32 @@
 from django.conf import settings
 from django.shortcuts import render
 
-
-def index_page(request):
-    context = {
+BASE_CONTEXT = {
         'title': settings.SITE_TITLE,
         'logo': settings.LOGO_TILE
     }
+
+
+def index_page(request):
+    context = BASE_CONTEXT.copy()
     return render(request, 'pages/index.html', context)
 
 
 def contact_page(request):
-    context = {
-        'title': settings.SITE_TITLE
-    }
+    context = BASE_CONTEXT.copy()
     return render(request, 'pages/contact.html', context)
 
 
 def about_page(request):
-    context = {
-        'title': settings.SITE_TITLE
-    }
+    context = BASE_CONTEXT.copy()
     return render(request, 'pages/about.html', context)
 
 
 def portfolio_page(request):
-    context = {
-        'title': settings.SITE_TITLE
-    }
+    context = BASE_CONTEXT.copy()
     return render(request, 'pages/portfolio.html', context)
 
 
 def services_page(request):
-    context = {
-        'title': settings.SITE_TITLE
-    }
+    context = BASE_CONTEXT.copy()
     return render(request, 'pages/services.html', context)
